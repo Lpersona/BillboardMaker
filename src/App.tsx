f281fed1582bf2868ff556593e58734981df4fa5
Persona <liusiyan92@qq.com>
2020-05-19 00:12:20 +0800

    添加基础取色板

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,39 @@
-import 'antd/dist/antd.css';
-import React from 'react';
-import './App.css';
-import Tool from './components/Tool';
-import SketchBoard from './components/SketchBoard';
-import PropertyBoard from './components/PropertyBoard';
-import { Layout } from 'antd';
-import ModalPanel from './components/ModalPanel';
+import "antd/dist/antd.css";
+import React from "react";
+import "./App.css";
+import Tool from "./components/Tool";
+import SketchBoard from "./components/SketchBoard";
+import PropertyBoard from "./components/PropertyBoard";
+import { Layout } from "antd";
+import ModalPanel from "./components/ModalPanel";
+import ColorBoard from "./components/ColorBoard";
 
 const { Sider, Content } = Layout;
 
 function App() {
-  return (
-    <div className="App">
-      <Layout
-        style={{
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        <Sider>
-          <Tool />
-        </Sider>
-        <Content>
-          <SketchBoard />
-        </Content>
-        <Sider>
-          <PropertyBoard />
-        </Sider>
-      </Layout>
+	return (
+		<div className="App">
+			<Layout
+				style={{
+					width: "100%",
+					height: "100%",
+				}}
+			>
+				<Sider>
+					<Tool />
+				</Sider>
+				<Content>
+					<SketchBoard />
+				</Content>
+				<Sider>
+					<PropertyBoard />
+				</Sider>
+			</Layout>
 
-      <ModalPanel />
-    </div>
-  );
+			<ModalPanel />
+			<ColorBoard />
+		</div>
+	);
 }
 
 export default App;
--- src/components/ColorBoard/index.tsx
+++ src/components/ColorBoard/index.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./index.scss";
 
 const ColorBoard: React.FC = () => {
-	return <></>;
+	const [colorString, setColorString] = useState("");
+	const [ctx, setCtx] = useState<CanvasRenderingContext2D>();
+
+	useEffect(() => {
+		const canvas = document.getElementById("color-board");
+		const ctx = (canvas as HTMLCanvasElement).getContext("2d");
+		if (ctx) {
+			setCtx(ctx);
+			for (var i = 0; i < 60; i++) {
+				for (var j = 0; j < 60; j++) {
+					for (var k = 0; k < 60; k++) {
+						ctx.fillStyle = `rgb(${Math.floor(255 - 4.25 * i)}, ${Math.floor(
+							255 - 4.25 * j,
+						)}, ${Math.floor(255 - 4.25 * k)})`;
+					}
+					ctx.fillRect(j * 5, i * 5, 5, 5);
+				}
+			}
+		}
+	}, []);
+
+	const handleGetColor = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+		e.persist();
+		const canvas = document.getElementById("color-board");
+		if (canvas) {
+			const rect = canvas.getBoundingClientRect();
+			const x = e.clientX - rect.left * ((canvas as HTMLCanvasElement).width / rect.width);
+			const y = e.clientY - rect.top * ((canvas as HTMLCanvasElement).height / rect.height);
+
+			const color = ctx?.getImageData(x, y, 1, 1);
+			console.log(color);
+		}
+	};
+
+	return (
+		<section className="color-board">
+			<canvas id="color-board" width={300} height={300} onClick={handleGetColor}></canvas>
+			<div className="value">{colorString}</div>
+		</section>
+	);
 };
 
 export default ColorBoard;
