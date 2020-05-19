d566d3b204c6e1d774af7162f4ec72613fd2b01b
Persona <liusiyan92@qq.com>
2020-05-20 07:51:49 +0800

    添加取色条

--- src/components/ColorBoard/index.tsx
+++ src/components/ColorBoard/index.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./index.scss";
+import { Slider } from "antd";
+import { SliderValue } from "antd/lib/slider";
 
 const ColorBoard: React.FC = () => {
 	const [colorString, setColorString] = useState("");
 	const [ctx, setCtx] = useState<CanvasRenderingContext2D>();
+	const [baseColor, setBaseColor] = useState(0);
 
 	useEffect(() => {
 		const canvas = document.getElementById("color-board");
@@ -11,17 +14,13 @@ const ColorBoard: React.FC = () => {
 		if (ctx) {
 			setCtx(ctx);
 			for (let i = 0; i < 60; i++) {
+				ctx.fillStyle = `rgb(${Math.floor(255 - 4.25 * i)}, ${baseColor}, ${baseColor})`;
 				for (let j = 0; j < 60; j++) {
-					for (let k = 0; k < 60; k++) {
-						ctx.fillStyle = `rgb(${Math.floor(255 - 4.25 * i)}, ${Math.floor(
-							255 - 4.25 * j,
-						)}, ${Math.floor(255 - 4.25 * k)})`;
-					}
 					ctx.fillRect(j * 5, i * 5, 5, 5);
 				}
 			}
 		}
-	}, []);
+	}, [baseColor]);
 
 	const handleGetColor = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>): void => {
 		e.persist();
@@ -31,14 +30,23 @@ const ColorBoard: React.FC = () => {
 			const x = e.clientX - rect.left * ((canvas as HTMLCanvasElement).width / rect.width);
 			const y = e.clientY - rect.top * ((canvas as HTMLCanvasElement).height / rect.height);
 
-			const color = ctx?.getImageData(x, y, 1, 1);
-			console.log(color);
+			const imageData = ctx?.getImageData(x, y, 1, 1);
+			setColorString(
+				`rgb(${imageData?.data[0]}, ${imageData?.data[1]}, ${imageData?.data[2]})`,
+			);
 		}
+	};
+
+	const handleBaseColorChange = (v: SliderValue) => {
+		setBaseColor(v as number);
 	};
 
 	return (
 		<section className="color-board">
 			<canvas id="color-board" width={300} height={300} onClick={handleGetColor}></canvas>
+			<section className={"color-slider"}>
+				<Slider defaultValue={255} max={255} min={0} onChange={handleBaseColorChange} />
+			</section>
 			<div className="value">{colorString}</div>
 		</section>
 	);
